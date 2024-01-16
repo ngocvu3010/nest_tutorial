65ab4f4a2554686f941dd70e50d05f87259c9712
Ngoc Vu <vu.thi.ngoc@sun-asterisk.com>
2024-01-16 14:08:37 +0700

    add ORM with sqlite3

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,21 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import {User} from "./user.entity"
 
 @Controller('app')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get('hello')
-  getHello(): string {
-    return this.appService.getHello();
+  async getHello(): Promise<User> {
+
+    return this.appService.createUser("ngoc vu");
+
+    // update
+    // const user = await this.appService.createUser("test");
+    // return this.appService.updateUser(user.id, "somthing elese");
+
+    // delete
+    // return this.appService.delete(1);
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,7 +1,47 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import {User} from './user.entity';
 
 @Injectable()
 export class AppService {
+
+  constructor(@InjectRepository(User) private usersRepository: Repository<User>) {}
+
+  getAll(): Promise<User[]> {
+    
+    return this.usersRepository.find({relations: ['pets']}); // select * from users join pets
+  }
+
+  async getOneById(id: number): Promise<User> {
+    try {
+      const user = this.usersRepository.findOneBy({id: id});
+      return user;
+    } catch (err) {
+      throw err;
+    }
+  }
+
+  createUser(name: string): Promise<User> {
+    const newUser = this.usersRepository.create({name}); // const newUser = new User(); newUser.name = name
+    return this.usersRepository.save(newUser); // insert
+  }
+
+  async updateUser(id: number, name: string): Promise<User> {
+    const user = await  this.getOneById(id);
+    user.name = name;
+    return this.usersRepository.save(user); // update
+  }
+
+  async deleteUser(id: number): Promise<User> {
+    const user = await  this.getOneById(id);
+    return this.usersRepository.remove(user); // delete
+  }
+
+  customeQuery(): any {
+    return this.usersRepository.createQueryBuilder("user").select("name").where("")
+  }
+
   getHello(): string {
     return 'Hello World!';
   }
